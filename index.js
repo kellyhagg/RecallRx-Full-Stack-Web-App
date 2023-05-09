08c894d7fb497bbfeedcaa9db93bd9338918b5cf
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2023-05-09 14:16:46 -0700

    Add require and const declarations from 2537

--- index.js
+++ index.js
@@ -1,3 +1,67 @@
+
+require("./utils.js");
+
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const bcrypt = require('bcrypt');
+const saltRounds = 12;
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+const Joi = require("joi");
+const mongoSanitize = require('express-mongo-sanitize');
+const { emit } = require("process");
+const { type } = require("os");
+const expireTime = 60 * 60 * 1000; //expires after 1 hour  (minutes * seconds * millis)
+
+/* secret information section */
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_database = process.env.MONGODB_DATABASE;
+const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
+const node_session_secret = process.env.NODE_SESSION_SECRET;
+/* END secret section */
+
+var { database } = include('databaseConnection');
+
+const userCollection = database.db(mongodb_database).collection('users');
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(mongoSanitize(
+    //{replaceWith: '%'}
+));
+
+// app.use(
+//     mongoSanitize({
+//       onSanitize: ({ req, key }) => {
+//         console.warn(`This request[${key}] is sanitized`);
+//       },
+//     }),
+//   );
+
+var mongoStore = MongoStore.create({
+    mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/sessions`,
+    crypto: {
+        secret: mongodb_session_secret
+    }
+})
+
+app.use(session({
+    secret: node_session_secret,
+    store: mongoStore, //default is memory store 
+    saveUninitialized: false,
+    resave: true
+}
+));
+
 app.get('/', (req, res) => {
     res.send("Hello World");
 });
