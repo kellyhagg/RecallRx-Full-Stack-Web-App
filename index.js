76845ad2b8491e28d0a2aeede71dec30564a8acd
Emma Lee <jy.lee.emma@gmail.com>
2023-05-10 14:44:46 -0700

    Add MMSE questionnaire files

---
+++ index.js
@@ -0,0 +1,72 @@
+require("./utils.js");
+
+const url = require('url');
+
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const ObjectId = require('mongodb').ObjectId;
+const usersModel = require('./models/w2users');
+const MongoStore = require('connect-mongo');
+const bcrypt = require('bcrypt');
+const saltRounds = 12;
+console.log(usersModel)
+const port = process.env.PORT || 4000;
+
+const app = express();
+
+const Joi = require("joi");
+
+const expireTime = 60 * 60 * 1000;  //expires after 1 hour  (hours * minutes * seconds * millis)
+
+/* secret information section */
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_database = process.env.MONGODB_DATABASE;
+const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
+
+console.log(mongodb_password)
+console.log(mongodb_user)
+const node_session_secret = process.env.NODE_SESSION_SECRET;
+/* END secret section */
+
+var { database } = include('databaseConnection');
+
+const userCollection = database.db(mongodb_database).collection('users');
+
+app.set('view engine', 'ejs');
+
+//req.body need this to parse (app.post) ex. req.body.username
+app.use(express.urlencoded({ extended: false }));
+
+// initially was /session, now /test in mongoURL
+var mongoStore = MongoStore.create({
+    mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/test`,
+    // mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/test`,
+    crypto: {
+        secret: mongodb_session_secret
+    }
+})
+
+//handles cookies. Ex. req.session.cookies. **would have to parse cookies ourselves otherwise.  
+app.use(session({
+    secret: node_session_secret,
+    store: mongoStore, //default is memory store 
+    saveUninitialized: false,
+    resave: true
+}
+));
+
+app.get('/', (req, res) => {
+    console.log(req.url);
+    console.log(url.parse(req.url));
+    res.render("mmse.ejs");
+});
+
+app.use(express.static(__dirname + "/public"));
+
+
+app.listen(port, () => {
+    console.log("Node application listening on port " + port);
+});
