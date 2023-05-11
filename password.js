f72de36d33c2c200b74c05bf0f1c35f37e0d8210
Emma Lee <jy.lee.emma@gmail.com>
2023-05-10 22:42:04 -0700

    Add email recovery pages for password reset

---
+++ password.js
@@ -0,0 +1,42 @@
+< !DOCTYPE html >
+    <html lang="en">
+
+        <head>
+            <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+                            integrity="sha384-
+        KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+                            <title>Document</title>
+                            <style>
+                                body {
+                                    //background-image: url('login.jpg');
+                                    background - position: center;
+                                height: 100vh;
+        }
+                            </style>
+                        </head>
+
+                        <body>
+                            <!--This code was on https://mdbootstrap.com/docs/standard/extended/login/ with adjustments and modifications made by Emma Lee -->
+                            <form>
+                                <div class="form-group">
+                                    <h1 style='text-align: center;'>Find your password </h1>
+                                    <h3 style=" text-align: center;"> Enter your recovery email </h3>
+                                </div>
+                                <div>
+
+                                    <input type="email" style="width: 100%; height: 50px" ; class="form-control" id="exampleInputEmail1"
+                aria-describedby="emailHelp" placeholder="Email">
+                                    <span style="font-size: 2em; color: #778251;">
+                                </div>
+                                <br>
+                                    <button type="next" class="btn dark-green-background pill white-font font-lg"
+                                        style="background-color: #778251;">
+                                        <h3>Next</h3>
+                                    </button>
+                            </form>
+                        </body>
+
+                    </html>
