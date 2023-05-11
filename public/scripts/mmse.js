cdd38b9cf2dc39797ffd9995866e8fe4c3502c18
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2023-05-10 23:56:56 -0700

    Add nextQuestion and verifySentence functions

--- public/scripts/mmse.js
+++ public/scripts/mmse.js
@@ -1,7 +1,34 @@
+var userScore = 0;
+var questionType;
+var sentence;
+
 // Function created by ChatGPT
 function speak() {
     var utterance = new SpeechSynthesisUtterance('The phone fell off of the shelf.');
     utterance.voice = speechSynthesis.getVoices()[0];
     speechSynthesis.speak(utterance);
 }
+
+function nextQuestionButton(questionType) {
+    console.log(questionType)
+    var userInput = document.getElementById("mmse-input").value;
+    if (questionType == "sentence-recall") {
+        console.log('The phone fell off of the shelf.');
+        verifySentence(userInput);
+    }
+}
+
+function verifySentence(sentence) {
+    // Provided by ChatGPT
+    let cleanStr = sentence.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").toLowerCase();
+    let cleanAnswer = "the phone fell off of the shelf";
+    console.log(cleanStr);
+    if (cleanStr == cleanAnswer) {
+        console.log("Correct!");
+        userScore++;
+    }
+    console.log(userScore);
+}
+
+console.log(questionType);
 
