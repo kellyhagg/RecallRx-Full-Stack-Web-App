231d2a6085c6551dc2da5443708cd591d7b7fd9d
Olga Zimina <olga.vic.zimina@gmail.com>
2023-05-13 15:42:17 -0700

    Create customize_email_validation_toast.js script for customizing email validation toast style.

---
+++ public/scripts/customize_email_validation_toast.js
@@ -0,0 +1,20 @@
+const input = document.querySelector('input[type="email"]');
+
+input.addEventListener("invalid", function (event) {
+  event.preventDefault();
+  if (!event.target.validity.valid) {
+    const validationMessage = event.target.validationMessage;
+    const toast = document.createElement("div");
+    toast.classList.add("custom-validation-message");
+    toast.textContent = validationMessage;
+    event.target.insertAdjacentElement("afterend", toast);
+  }
+});
+
+input.addEventListener("input", function (event) {
+  const toast = event.target.nextElementSibling;
+  if (toast && toast.classList.contains("custom-validation-message")) {
+    toast.remove();
+  }
+  event.target.setCustomValidity("");
+});
