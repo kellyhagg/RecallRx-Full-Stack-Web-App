92be30a40d8e8711982435fc797df5db6061f569
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2023-05-12 20:06:07 -0700

    Add proper routes for mmse ejs files

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ require("./utils.js");
 require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
+const bodyParser = require('body-parser');
+
 const MongoStore = require('connect-mongo');
 const bcrypt = require('bcrypt');
 const saltRounds = 12;
 
 const port = process.env.PORT || 3000;
+const number = 1
 
 const app = express();
 
@@ -16,6 +19,7 @@ const Joi = require("joi");
 const mongoSanitize = require('express-mongo-sanitize');
 const { emit } = require("process");
 const { type } = require("os");
+const { getOrientationScore } = require("./public/scripts/mmse.js");
 const expireTime = 60 * 60 * 1000; //expires after 1 hour  (minutes * seconds * millis)
 
 /* secret information section */
@@ -27,13 +31,25 @@ const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
 const node_session_secret = process.env.NODE_SESSION_SECRET;
 /* END secret section */
 
+const mmse = require('./public/scripts/mmse.js');
+
 var { database } = include('databaseConnection');
 
 const userCollection = database.db(mongodb_database).collection('users');
 
+// file1.js
+const myGlobalVar = "Hello, world!";
+
+let userScore = 0;
+
+module.exports = userScore;
+
+let pageCount = 1;
+
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(mongoSanitize(
     //{replaceWith: '%'}
@@ -62,8 +78,81 @@ app.use(session({
 }
 ));
 
+app.use(express.static("public"));
+
+
 app.get('/', (req, res) => {
-    res.render("templates/header");
+    res.redirect('/mmse-landing-page');
+});
+
+app.post('/', async (req, res) => {
+    res.redirect('/mmse-landing-page');
+});
+
+app.get('/mmse-landing-page', (req, res) => {
+    res.render("mmse-landing-page", { headerMessage: "MMSE Questionnaire" });
+});
+
+app.post('/mmse-landing-page', async (req, res) => {
+    res.redirect('/mmse-orientation');
+});
+
+app.get('/mmse-orientation', (req, res) => {
+    res.render("mmse-orientation.ejs", { headerMessage: "MMSE Questionnaire" });
+});
+
+app.post('/mmse-orientation', async (req, res) => {
+    var year1 = req.body.year1;
+    var year2 = req.body.year2;
+    var year3 = req.body.year3;
+    var year4 = req.body.year4;
+    var month = req.body.month;
+    var day = req.body.day;
+
+    let result = year1 + year2 + year3 + year4 + month + day;
+
+    userScore = userScore + await getOrientationScore(result);
+    res.render("mmse-object-recall.ejs", { headerMessage: "MMSE Questionnaire", objectName: "scissors", pageCount: pageCount++ });
+    console.log("userScore: " + userScore);
+    return;
+});
+
+app.get('/mmse-object-recall', (req, res) => {
+    res.render("mmse-object-recall.ejs", { headerMessage: "MMSE Questionnaire", objectName: "scissors", pageCount: pageCount++ });
+});
+
+app.post('/mmse-object-recall', async (req, res) => {
+    if (pageCount <= 3) {
+        res.redirect('/mmse-object-recall');
+    } else {
+        pageCount = 1;
+        res.redirect('/mmse-sentence-recall');
+    }
+});
+
+app.get('/mmse-sentence-recall', (req, res) => {
+    res.render("mmse-sentence-recall.ejs", { headerMessage: "MMSE Questionnaire" });
+});
+
+app.post('/mmse-sentence-recall', async (req, res) => {
+    res.redirect('/mmse-word-reversal');
+});
+
+app.get('/mmse-word-reversal', (req, res) => {
+    res.render("mmse-word-reversal.ejs", { headerMessage: "MMSE Questionnaire", word: "hello", pageCount: pageCount++ });
+});
+
+app.post('/mmse-word-reversal', async (req, res) => {
+    if (pageCount <= 3) {
+        res.redirect('/mmse-word-reversal');
+    } else {
+        pageCount = 1;
+        res.redirect('/mmse-results');
+    }
+});
+
+app.get('/mmse-results', (req, res) => {
+    res.render("mmse-results.ejs", { headerMessage: "MMSE Results" });
 });
 
 app.listen(port, () => {
