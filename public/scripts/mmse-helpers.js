aa4d3ee2265d9d3f4db09e38f05b9c5536f01977
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2023-05-13 18:02:30 -0700

    Add eventlistener to disable sentenceSpeakBtn

--- public/scripts/mmse-helpers.js
+++ public/scripts/mmse-helpers.js
@@ -1,6 +1,14 @@
+
+const button = document.getElementById("sentenceSpeakBtn");
+
+button.addEventListener("click", function () {
+    button.disabled = true;
+});
+
 // Function created by ChatGPT
 function speak(phrase) {
     let utterance = new SpeechSynthesisUtterance(phrase);
     utterance.voice = speechSynthesis.getVoices()[0];
     speechSynthesis.speak(utterance);
 }
+
