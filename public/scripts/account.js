27e24470ec6d1bc18c9ad7726110fbd822e398a7
Olga Zimina <olga.vic.zimina@gmail.com>
2023-05-14 02:16:03 -0700

    Implement script for applying custom styles to password validation toasts

--- public/scripts/account.js
+++ public/scripts/account.js
@@ -26,3 +26,44 @@ console.log(confirmPassword.value);
 
 password.onblur = checkPasswordsMatch;
 confirmPassword.onblur = checkPasswordsMatch;
+
+const passwordInput = document.querySelector("#password");
+const confirmPasswordInput = document.querySelector("#confirmPassword");
+
+passwordInput.addEventListener("invalid", function (event) {
+  event.preventDefault();
+  if (!event.target.validity.valid) {
+    const validationMessage = event.target.validationMessage;
+    const toast = document.createElement("div");
+    toast.classList.add("custom-validation-message");
+    toast.textContent = validationMessage;
+    event.target.insertAdjacentElement("afterend", toast);
+  }
+});
+
+passwordInput.addEventListener("input", function (event) {
+  const toast = event.target.nextElementSibling;
+  if (toast && toast.classList.contains("custom-validation-message")) {
+    toast.remove();
+  }
+  event.target.setCustomValidity("");
+});
+
+confirmPasswordInput.addEventListener("invalid", function (event) {
+  event.preventDefault();
+  if (!event.target.validity.valid) {
+    const validationMessage = event.target.validationMessage;
+    const toast = document.createElement("div");
+    toast.classList.add("custom-validation-message");
+    toast.textContent = validationMessage;
+    event.target.insertAdjacentElement("afterend", toast);
+  }
+});
+
+confirmPasswordInput.addEventListener("input", function (event) {
+  const toast = event.target.nextElementSibling;
+  if (toast && toast.classList.contains("custom-validation-message")) {
+    toast.remove();
+  }
+  event.target.setCustomValidity("");
+});
