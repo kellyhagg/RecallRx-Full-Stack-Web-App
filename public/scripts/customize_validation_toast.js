dcbab993cbbbd4d71b3068c6db45e1b553ae48fc
Olga Zimina <olga.vic.zimina@gmail.com>
2023-05-14 02:21:19 -0700

    Implement script to customize error messages on user information update pages.

---
+++ public/scripts/customize_validation_toast.js
@@ -0,0 +1,27 @@
+var input = "";
+if (document.querySelector("#password")) {
+  input = document.querySelector("#password");
+} else if (document.querySelector('input[type="email"]')) {
+  input = document.querySelector('input[type="email"]');
+} else if (document.querySelector("#name")) {
+  input = ocument.querySelector("#name");
+}
+
+input.addEventListener("invalid", function (event) {
+  event.preventDefault();
+  if (!event.target.validity.valid) {
+    const validationMessage = event.target.validationMessage;
+    const toast = document.createElement("div");
+    toast.classList.add("custom-validation-message");
+    toast.textContent = validationMessage;
+    event.target.insertAdjacentElement("afterend", toast);
+  }
+});
+
+input.addEventListener("input", function (event) {
+  const toast = event.target.nextElementSibling;
+  if (toast && toast.classList.contains("custom-validation-message")) {
+    toast.remove();
+  }
+  event.target.setCustomValidity("");
+});
