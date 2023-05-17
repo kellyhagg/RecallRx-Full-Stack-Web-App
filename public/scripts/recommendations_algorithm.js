dd9aac8f9a0bfaf0f2704ae83806c3296a0286b3
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2023-05-17 13:27:58 -0700

    Get risk increase per cigarette per day

--- public/scripts/recommendations_algorithm.js
+++ public/scripts/recommendations_algorithm.js
@@ -1,13 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 
+const smokingRiskIncrease = 0.37;
+
+// Provided by ChatGPT
 function calculateAverageByKey(data, key) {
-    const numbers = data.map(item => item[key]);
+    const filteredData = data.filter(item => item.hasOwnProperty(key));
+    const numbers = filteredData.map(item => item[key]);
     const sum = numbers.reduce((acc, num) => acc + num, 0);
     const average = sum / numbers.length;
     return average;
 }
 
+// Made with the assistance of ChatGPT
 function readJSONFile(filename, callback) {
     const filePath = path.join(__dirname, '..', filename);
     fs.readFile(filePath, 'utf8', (error, data) => {
@@ -15,27 +20,25 @@ function readJSONFile(filename, callback) {
             console.log('Error occurred while reading the JSON file:', error);
             return;
         }
-
-        // Parse the JSON data
         const jsonData = JSON.parse(data);
-
-        // Invoke the callback function with the retrieved data
         callback(jsonData);
     });
+}
+
+function getSlope(x1, y1, x2, y2) {
+    return (y2 - y1) / (x2 - x1);
 }
 
 function runAlgorithm() {
     const filename = 'resources/cigarette_relative_risk_data.json';
 
-    // Pass a callback function to readJSONFile to process the retrieved data
     readJSONFile(filename, (data) => {
         const average1 = calculateAverageByKey(data, "1");
         const average20 = calculateAverageByKey(data, "20");
 
-        const result = [average1, average20];
-        console.log(result);
+        const riskPerCigarette = smokingRiskIncrease * getSlope(1, average1, 20, average20);
+        console.log(riskPerCigarette);
     });
 }
 
-// Call the function to run the algorithm
 runAlgorithm();
