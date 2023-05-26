1cf639aae775463908146c4fab8018978a726415
Olga Zimina <olga.vic.zimina@gmail.com>
2023-05-26 03:16:28 -0700

    Add comments to the customize_passworrd_validation_toast.js.

--- public/scripts/customized_password_validation_toast.js
+++ public/scripts/customized_password_validation_toast.js
@@ -1,8 +1,11 @@
+// Get the password and confirm password input elements
 const passwordInput = document.querySelector("#password");
 const confirmPasswordInput = document.querySelector("#confirmPassword");
 
+// Add event listener for invalid input for the "Password" field
 passwordInput.addEventListener("invalid", function (event) {
-  event.preventDefault();
+  event.preventDefault(); // Prevent the default browser validation behavior
+  // Check if the input is not valid
   if (!event.target.validity.valid) {
     const validationMessage = event.target.validationMessage;
     const toast = document.createElement("div");
@@ -12,16 +15,19 @@ passwordInput.addEventListener("invalid", function (event) {
   }
 });
 
+// Add event listener for "input" event
 passwordInput.addEventListener("input", function (event) {
   const toast = event.target.nextElementSibling;
   if (toast && toast.classList.contains("custom-validation-message")) {
     toast.remove();
   }
-  event.target.setCustomValidity("");
+  event.target.setCustomValidity(""); // Reset the custom validity message
 });
 
+// Add event listener for invalid input for the "Confirm Password" field
 confirmPasswordInput.addEventListener("invalid", function (event) {
-  event.preventDefault();
+  event.preventDefault(); // Prevent the default browser validation behavior
+  // Check if the input is not valid
   if (!event.target.validity.valid) {
     const validationMessage = event.target.validationMessage;
     const toast = document.createElement("div");
@@ -31,10 +37,11 @@ confirmPasswordInput.addEventListener("invalid", function (event) {
   }
 });
 
+// Add event listener for "input" event on the "Confirm Password" field
 confirmPasswordInput.addEventListener("input", function (event) {
   const toast = event.target.nextElementSibling;
   if (toast && toast.classList.contains("custom-validation-message")) {
-    toast.remove();
+    toast.remove(); // Remove the toast validation message if it exists
   }
-  event.target.setCustomValidity("");
+  event.target.setCustomValidity(""); // Reset the custom validity message
 });
