88e582d96d502cfb935c51d1c25e639833b3b555
Olga Zimina <olga.vic.zimina@gmail.com>
2023-05-26 03:14:59 -0700

    Add comments to customize_email_validation_toast.js

--- public/scripts/customize_email_validation_toast.js
+++ public/scripts/customize_email_validation_toast.js
@@ -1,11 +1,15 @@
+// Get the email input element
 const emailInput = document.querySelector('input[type="email"]');
 
+// Add event listener for "invalid" event
 emailInput.addEventListener("invalid", function (event) {
   event.preventDefault();
+  // Check if the input is not valid
   if (!event.target.validity.valid) {
     const validationMessage = event.target.validationMessage;
     const toast = document.createElement("div");
     toast.classList.add("custom-validation-message");
+    // Customize the validation message for Safari
     if (validationMessage === "Enter an email address") {
       validationMessage = "Please enter a valid email address.";
     }
@@ -14,10 +18,12 @@ emailInput.addEventListener("invalid", function (event) {
   }
 });
 
+// Add event listener for "input" event
 emailInput.addEventListener("input", function (event) {
   const toast = event.target.nextElementSibling;
+  // Remove the custom validation message if it exists
   if (toast && toast.classList.contains("custom-validation-message")) {
     toast.remove();
   }
-  event.target.setCustomValidity("");
+  event.target.setCustomValidity(""); // Reset the custom validity message
 });
