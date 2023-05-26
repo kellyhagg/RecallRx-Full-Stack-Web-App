6bff55cc4b7ed20f3f601a2e1d2877b961113d37
Olga Zimina <olga.vic.zimina@gmail.com>
2023-05-26 03:15:33 -0700

    Add comments to the customize_login_form_validation_toast.js.

--- public/scripts/customize_login_form_validation_toast.js
+++ public/scripts/customize_login_form_validation_toast.js
@@ -1,6 +1,9 @@
+// Get the password input element
 const passwordInput = document.querySelector("#floatingPassword");
+// Get the username input element// Get the password input element
 const userNameInput = document.querySelector("#floatingInput");
 
+// Remove the toast validation message
 function removeToast(element) {
   const toast = element.nextElementSibling;
   if (toast && toast.classList.contains("custom-validation-message")) {
@@ -8,37 +11,44 @@ function removeToast(element) {
   }
 }
 
+// Add event listener for password input when it becomes invalid
 passwordInput.addEventListener("invalid", function (event) {
-  event.preventDefault();
-  removeToast(event.target);
+  event.preventDefault(); // Prevent the default browser validation behavior
+  removeToast(event.target); // Remove any existing validation message toast
+
+  // Check if the input is not valid
   if (!event.target.validity.valid) {
     const validationMessage = event.target.validationMessage;
     const toast = document.createElement("div");
-    toast.classList.add("custom-validation-message");
-    toast.textContent = validationMessage;
-    event.target.insertAdjacentElement("afterend", toast);
+    toast.classList.add("custom-validation-message"); // Add a class for styling
+    toast.textContent = validationMessage; // Set the validation message text
+    event.target.insertAdjacentElement("afterend", toast); // Insert the toast element after the input
   }
 });
 
+// Add event listener for password input on input change
 passwordInput.addEventListener("input", function (event) {
-  removeToast(event.target);
-  event.target.setCustomValidity("");
+  removeToast(event.target); // Remove any existing validation message toast
+  event.target.setCustomValidity(""); // Reset the custom validation message
 });
 
+// Add event listener for username input when it becomes invalid
 userNameInput.addEventListener("invalid", function (event) {
-  event.preventDefault();
+  event.preventDefault(); // Prevent the default browser validation behavior
+  removeToast(event.target); // Remove any existing validation message toast
 
-  removeToast(event.target);
+  // Check if the input is not valid
   if (!event.target.validity.valid) {
     const validationMessage = event.target.validationMessage;
     const toast = document.createElement("div");
-    toast.classList.add("custom-validation-message");
-    toast.textContent = validationMessage;
-    event.target.insertAdjacentElement("afterend", toast);
+    toast.classList.add("custom-validation-message"); // Add a class for styling
+    toast.textContent = validationMessage; // Set the validation message text
+    event.target.insertAdjacentElement("afterend", toast); // Insert the toast element after the input
   }
 });
 
+// Add event listener for username input on input change
 userNameInput.addEventListener("input", function (event) {
-  removeToast(event.target);
-  event.target.setCustomValidity("");
+  removeToast(event.target); // Remove any existing validation message toast
+  event.target.setCustomValidity(""); // Reset the custom validation message
 });
